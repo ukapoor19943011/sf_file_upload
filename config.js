384de31b228260e81cf3b33bb8290aec9cc83dff
Utsav Kapoor <utsav@Utsavs-MacBook-Air.local>
2018-07-13 22:07:51 +0530

    newww

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // All configuration options
 
 module.exports.port = process.env.PORT || 3000; // App's port
-module.exports.aws_key = process.env.AWS_KEY || "AKIAJ7FAQZI6NLZVPNHQ"; // AWS Key
-module.exports.aws_secret = process.env.AWS_SECRET || "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"; // AWS Secret
+module.exports.aws_key = process.env.AWS_KEY || "AKIAJFUFN7UPAJ56RNUQ"; // AWS Key
+module.exports.aws_secret = process.env.AWS_SECRET || "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"; // AWS Secret
 module.exports.aws_bucket = process.env.AWS_BUCKET || "large-file-upload-two"; // AWS Bucket
 module.exports.redirect_host = process.env.REDIRECT_HOST || "http://localhost:3000/"; // Host to redirect after uploading
 module.exports.host = process.env.HOST ||  "s3.amazonaws.com"; // S3 provider host
